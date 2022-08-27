0b873e809e40adedb3de8cc57c6f8c3ea8cfa6f9
vovanone2005 <voevoda.junior@mail.ru>
2022-08-27 08:43:17 +0300

    Task_13

---
+++ Семинар_3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число ");
+Console.Write("a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int a1 = a / 100;
+int a2 = a %10;
+if (a < 99)
+{
+    Console.WriteLine("Тертьей цифы нет");
+}
+else
+{
+    Console.WriteLine( a2 );
+}
